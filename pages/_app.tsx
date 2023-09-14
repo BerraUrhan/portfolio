f7608b69938b0fef2c5d8e1e0c34cf851c5572ef
BerraUrhan <berraurhan2@gmail.com>
2023-09-14 11:13:12 +0300

    updated size favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,8 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       {/* Add the favicon */}
       <Head>
-        <link rel="shortcut icon" href="/favicon.ico" sizes="16x16" />
-        <link rel="icon" href="/favicon.ico" sizes="16x16"/>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Add the favicon */}
       {/* Note that the path doesn't include "public" */}
